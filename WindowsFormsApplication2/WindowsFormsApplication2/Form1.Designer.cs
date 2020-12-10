fd3c08de1305ddeb6458bb93370de029896459be
Ramirez5034 <70988794+Ramirez5034@users.noreply.github.com>
2020-12-09 18:57:18 -0600

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numDato1 = new System.Windows.Forms.NumericUpDown();
+            this.btnAreaCuadrado = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.numDato2 = new System.Windows.Forms.NumericUpDown();
+            this.btnAreaTriangulo = new System.Windows.Forms.Button();
+            this.lblTriangulo = new System.Windows.Forms.Label();
+            this.numdato4 = new System.Windows.Forms.NumericUpDown();
+            this.numDato5 = new System.Windows.Forms.NumericUpDown();
+            this.btnRectangulo = new System.Windows.Forms.Button();
+            this.lblRectangulo = new System.Windows.Forms.Label();
+            this.numDato3 = new System.Windows.Forms.NumericUpDown();
+            this.numDato6 = new System.Windows.Forms.NumericUpDown();
+            this.numDato7 = new System.Windows.Forms.NumericUpDown();
+            this.btnRombo = new System.Windows.Forms.Button();
+            this.lblRombo = new System.Windows.Forms.Label();
+            this.numDato8 = new System.Windows.Forms.NumericUpDown();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.lblCirculo = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numdato4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numDato1
+            // 
+            this.numDato1.DecimalPlaces = 2;
+            this.numDato1.Location = new System.Drawing.Point(12, 186);
+            this.numDato1.Name = "numDato1";
+            this.numDato1.Size = new System.Drawing.Size(120, 20);
+            this.numDato1.TabIndex = 0;
+            // 
+            // btnAreaCuadrado
+            // 
+            this.btnAreaCuadrado.Location = new System.Drawing.Point(164, 166);
+            this.btnAreaCuadrado.Name = "btnAreaCuadrado";
+            this.btnAreaCuadrado.Size = new System.Drawing.Size(81, 57);
+            this.btnAreaCuadrado.TabIndex = 1;
+            this.btnAreaCuadrado.Text = "Area del cuadrado";
+            this.btnAreaCuadrado.UseVisualStyleBackColor = true;
+            this.btnAreaCuadrado.Click += new System.EventHandler(this.btnArea_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(264, 188);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(15, 13);
+            this.lblArea.TabIndex = 2;
+            this.lblArea.Text = "R";
+            // 
+            // numDato2
+            // 
+            this.numDato2.DecimalPlaces = 2;
+            this.numDato2.Location = new System.Drawing.Point(295, 203);
+            this.numDato2.Name = "numDato2";
+            this.numDato2.Size = new System.Drawing.Size(120, 20);
+            this.numDato2.TabIndex = 3;
+            // 
+            // btnAreaTriangulo
+            // 
+            this.btnAreaTriangulo.Location = new System.Drawing.Point(421, 166);
+            this.btnAreaTriangulo.Name = "btnAreaTriangulo";
+            this.btnAreaTriangulo.Size = new System.Drawing.Size(81, 57);
+            this.btnAreaTriangulo.TabIndex = 4;
+            this.btnAreaTriangulo.Text = "Area del triangulo";
+            this.btnAreaTriangulo.UseVisualStyleBackColor = true;
+            this.btnAreaTriangulo.Click += new System.EventHandler(this.btnAreaTriangulo_Click);
+            // 
+            // lblTriangulo
+            // 
+            this.lblTriangulo.AutoSize = true;
+            this.lblTriangulo.Location = new System.Drawing.Point(525, 193);
+            this.lblTriangulo.Name = "lblTriangulo";
+            this.lblTriangulo.Size = new System.Drawing.Size(15, 13);
+            this.lblTriangulo.TabIndex = 8;
+            this.lblTriangulo.Text = "R";
+            // 
+            // numdato4
+            // 
+            this.numdato4.DecimalPlaces = 2;
+            this.numdato4.Location = new System.Drawing.Point(575, 177);
+            this.numdato4.Name = "numdato4";
+            this.numdato4.Size = new System.Drawing.Size(120, 20);
+            this.numdato4.TabIndex = 9;
+            // 
+            // numDato5
+            // 
+            this.numDato5.DecimalPlaces = 2;
+            this.numDato5.Location = new System.Drawing.Point(575, 203);
+            this.numDato5.Name = "numDato5";
+            this.numDato5.Size = new System.Drawing.Size(120, 20);
+            this.numDato5.TabIndex = 10;
+            // 
+            // btnRectangulo
+            // 
+            this.btnRectangulo.Location = new System.Drawing.Point(701, 171);
+            this.btnRectangulo.Name = "btnRectangulo";
+            this.btnRectangulo.Size = new System.Drawing.Size(81, 57);
+            this.btnRectangulo.TabIndex = 11;
+            this.btnRectangulo.Text = "Area del rectangulo";
+            this.btnRectangulo.UseVisualStyleBackColor = true;
+            this.btnRectangulo.Click += new System.EventHandler(this.btnRectangulo_Click);
+            // 
+            // lblRectangulo
+            // 
+            this.lblRectangulo.AutoSize = true;
+            this.lblRectangulo.Location = new System.Drawing.Point(788, 193);
+            this.lblRectangulo.Name = "lblRectangulo";
+            this.lblRectangulo.Size = new System.Drawing.Size(15, 13);
+            this.lblRectangulo.TabIndex = 12;
+            this.lblRectangulo.Text = "R";
+            // 
+            // numDato3
+            // 
+            this.numDato3.DecimalPlaces = 2;
+            this.numDato3.Location = new System.Drawing.Point(295, 177);
+            this.numDato3.Name = "numDato3";
+            this.numDato3.Size = new System.Drawing.Size(120, 20);
+            this.numDato3.TabIndex = 7;
+            // 
+            // numDato6
+            // 
+            this.numDato6.AccessibleName = "";
+            this.numDato6.DecimalPlaces = 2;
+            this.numDato6.Location = new System.Drawing.Point(141, 398);
+            this.numDato6.Name = "numDato6";
+            this.numDato6.Size = new System.Drawing.Size(120, 20);
+            this.numDato6.TabIndex = 14;
+            // 
+            // numDato7
+            // 
+            this.numDato7.AccessibleName = "";
+            this.numDato7.DecimalPlaces = 2;
+            this.numDato7.Location = new System.Drawing.Point(141, 424);
+            this.numDato7.Name = "numDato7";
+            this.numDato7.Size = new System.Drawing.Size(120, 20);
+            this.numDato7.TabIndex = 15;
+            // 
+            // btnRombo
+            // 
+            this.btnRombo.Location = new System.Drawing.Point(267, 387);
+            this.btnRombo.Name = "btnRombo";
+            this.btnRombo.Size = new System.Drawing.Size(81, 64);
+            this.btnRombo.TabIndex = 16;
+            this.btnRombo.Text = "Area del rombo";
+            this.btnRombo.UseVisualStyleBackColor = true;
+            this.btnRombo.Click += new System.EventHandler(this.btnRombo_Click);
+            // 
+            // lblRombo
+            // 
+            this.lblRombo.AutoSize = true;
+            this.lblRombo.Location = new System.Drawing.Point(354, 413);
+            this.lblRombo.Name = "lblRombo";
+            this.lblRombo.Size = new System.Drawing.Size(15, 13);
+            this.lblRombo.TabIndex = 17;
+            this.lblRombo.Text = "R";
+            // 
+            // numDato8
+            // 
+            this.numDato8.AccessibleName = "";
+            this.numDato8.DecimalPlaces = 2;
+            this.numDato8.Location = new System.Drawing.Point(421, 413);
+            this.numDato8.Name = "numDato8";
+            this.numDato8.Size = new System.Drawing.Size(120, 20);
+            this.numDato8.TabIndex = 19;
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.Location = new System.Drawing.Point(547, 387);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(81, 64);
+            this.btnCirculo.TabIndex = 21;
+            this.btnCirculo.Text = "Area del circulo";
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            this.btnCirculo.Click += new System.EventHandler(this.btnCirculo_Click);
+            // 
+            // lblCirculo
+            // 
+            this.lblCirculo.AutoSize = true;
+            this.lblCirculo.Location = new System.Drawing.Point(634, 413);
+            this.lblCirculo.Name = "lblCirculo";
+            this.lblCirculo.Size = new System.Drawing.Size(15, 13);
+            this.lblCirculo.TabIndex = 22;
+            this.lblCirculo.Text = "R";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApplication2.Properties.Resources.circulo;
+            this.pictureBox5.Location = new System.Drawing.Point(459, 229);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(146, 140);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 23;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApplication2.Properties.Resources.rombo;
+            this.pictureBox4.Location = new System.Drawing.Point(141, 229);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(196, 140);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 18;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApplication2.Properties.Resources.rectangulo;
+            this.pictureBox3.Location = new System.Drawing.Point(554, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(215, 148);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication2.Properties.Resources.triangulo;
+            this.pictureBox2.Location = new System.Drawing.Point(298, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(174, 148);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication2.Properties.Resources.cuadrado_2;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 524);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.lblCirculo);
+            this.Controls.Add(this.btnCirculo);
+            this.Controls.Add(this.numDato8);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lblRombo);
+            this.Controls.Add(this.btnRombo);
+            this.Controls.Add(this.numDato7);
+            this.Controls.Add(this.numDato6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.lblRectangulo);
+            this.Controls.Add(this.btnRectangulo);
+            this.Controls.Add(this.numDato5);
+            this.Controls.Add(this.numdato4);
+            this.Controls.Add(this.lblTriangulo);
+            this.Controls.Add(this.numDato3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAreaTriangulo);
+            this.Controls.Add(this.numDato2);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.btnAreaCuadrado);
+            this.Controls.Add(this.numDato1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numDato1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numdato4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDato8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numDato1;
+        private System.Windows.Forms.Button btnAreaCuadrado;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.NumericUpDown numDato2;
+        private System.Windows.Forms.Button btnAreaTriangulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblTriangulo;
+        private System.Windows.Forms.NumericUpDown numdato4;
+        private System.Windows.Forms.NumericUpDown numDato5;
+        private System.Windows.Forms.Button btnRectangulo;
+        private System.Windows.Forms.Label lblRectangulo;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NumericUpDown numDato3;
+        private System.Windows.Forms.NumericUpDown numDato6;
+        private System.Windows.Forms.NumericUpDown numDato7;
+        private System.Windows.Forms.Button btnRombo;
+        private System.Windows.Forms.Label lblRombo;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.NumericUpDown numDato8;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Label lblCirculo;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            CAreas a = new CAreas();
+            decimal resultado = a.Cuadrado(numDato1.Value);
+            lblArea.Text = resultado + ""; 
+        }
+
+        private void btnAreaTriangulo_Click(object sender, EventArgs e)
+        {
+
+            CAreas a = new CAreas();
+            decimal resultado = a.Triangulo(numDato3.Value, numDato2.Value);
+            lblTriangulo.Text = resultado + "";
+        
+        }
+
+        private void btnRectangulo_Click(object sender, EventArgs e)
+        {
+
+            CAreas a = new CAreas();
+            decimal resultado = a.Rectangulo(numdato4.Value, numDato5.Value);
+            lblRectangulo.Text = resultado + ""; 
+        }
+
+        private void btnRombo_Click(object sender, EventArgs e)
+        {
+            CAreas a = new CAreas();
+            decimal resultado = a.Rombo(numDato6.Value, numDato7.Value);
+            lblRombo.Text = resultado + ""; 
+        }
+
+        private void btnCirculo_Click(object sender, EventArgs e)
+        {
+            CAreas a = new CAreas();
+            decimal resultado = a.Circulo(numDato8.Value); 
+            lblCirculo.Text = resultado + "";
+        }
+    }
+}
